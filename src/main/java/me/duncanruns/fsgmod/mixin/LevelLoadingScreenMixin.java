2c40651ef59f133a1803a455355586a332b11ddc
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2024-07-25 13:06:48 -0300

    Hide seed with some dank ass mixins

---
+++ src/main/java/me/duncanruns/fsgmod/mixin/LevelLoadingScreenMixin.java
@@ -0,0 +1,29 @@
+package me.duncanruns.fsgmod.mixin;
+
+import com.bawnorton.mixinsquared.TargetHandler;
+import net.minecraft.client.gui.screen.LevelLoadingScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(value = LevelLoadingScreen.class, priority = 1500)
+public abstract class LevelLoadingScreenMixin extends Screen {
+    protected LevelLoadingScreenMixin(Text title) {
+        super(title);
+    }
+
+    /**
+     * @author tildejustin
+     * @author DuncanRuns
+     */
+    @TargetHandler(
+            mixin = "me.voidxwalker.autoreset.mixin.LevelLoadingScreenMixin",
+            name = "atum_modifyString"
+    )
+    @ModifyArg(method = "@MixinSquared:Handler", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/LevelLoadingScreen;drawCenteredString(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V"))
+    private String replaceAtumRenderText(String ignore) {
+        return "Filtered Seed";
+    }
+}
