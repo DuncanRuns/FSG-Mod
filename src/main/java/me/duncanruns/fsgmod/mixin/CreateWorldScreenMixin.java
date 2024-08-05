e001b4b81da2cf0ecf4f006e8062fa1915b93fb1
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2024-08-05 14:27:58 -0300

    Update CreateWorldScreenMixin.java

--- src/main/java/me/duncanruns/fsgmod/mixin/CreateWorldScreenMixin.java
+++ src/main/java/me/duncanruns/fsgmod/mixin/CreateWorldScreenMixin.java
@@ -1,5 +1,6 @@
 package me.duncanruns.fsgmod.mixin;
 
+import com.bawnorton.mixinsquared.TargetHandler;
 import me.duncanruns.fsgmod.FSGFilterResult;
 import me.duncanruns.fsgmod.FSGMod;
 import me.duncanruns.fsgmod.SeedManager;
@@ -8,12 +9,14 @@ import me.duncanruns.fsgmod.screen.FilteringScreen;
 import me.voidxwalker.autoreset.Atum;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.world.CreateWorldScreen;
+import org.apache.logging.log4j.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(CreateWorldScreen.class)
+@Mixin(value = CreateWorldScreen.class, priority = 1500)
 public abstract class CreateWorldScreenMixin extends Screen {
     protected CreateWorldScreenMixin() {
         super(null);
@@ -38,5 +41,15 @@ public abstract class CreateWorldScreenMixin extends Screen {
             minecraft.openScreen(new FilteringScreen());
             info.cancel();
         }
+    }
+
+
+    @TargetHandler(
+            mixin = "me.voidxwalker.autoreset.mixin.CreateWorldScreenMixin",
+            name = "injected"
+    )
+    @Redirect(method = "@MixinSquared:Handler", at = @At(value = "INVOKE", target = "Lme/voidxwalker/autoreset/Atum;log(Lorg/apache/logging/log4j/Level;Ljava/lang/String;)V"))
+    private void replaceAtumLog(Level level, String message) {
+        Atum.log(level, message.startsWith("Resetting the set seed") ? "Resetting a filtered seed" : message);
     }
 }
