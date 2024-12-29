7781248bcd2af0764be4f1bb2783ba0c8dec0b74
Duncan <59705125+DuncanRuns@users.noreply.github.com>
2024-12-29 07:12:58 -0400

    Delete MinecraftClientMixin.java

--- src/main/java/me/duncanruns/fsgmod/mixin/MinecraftClientMixin.java
+++
@@ -1,9 +0,0 @@
-package me.duncanruns.fsgmod.mixin;
-
-import net.minecraft.client.MinecraftClient;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(MinecraftClient.class)
-public abstract class MinecraftClientMixin {
-
-}
